eb76a2d8f5311a8e405812fb71f4777ca32731a4
LionyxML <rahul.juliato@gmail.com>
2021-11-07 23:05:11 -0300

    Added endpoints / cleaned db

--- api_sequelize_sqlite/index.js
+++ api_sequelize_sqlite/index.js
@@ -14,6 +14,31 @@ app.post("/users", async (req, res) => {
   res.send("User is inserted");
 });
 
+app.get("/users", async (req, res) => {
+  const users = await User.findAll();
+  res.send(users);
+});
+
+app.get("/users/:id", async (req, res) => {
+  const requestedId = req.params.id;
+  const user = await User.findOne({ where: { id: requestedId } });
+  res.send(user);
+});
+
+app.put("/users/:id", async (req, res) => {
+  const requestedId = req.params.id;
+  const user = await User.findOne({ where: { id: requestedId } });
+  user.username = req.body.username;
+  await user.save();
+  res.send("Updated");
+});
+
+app.delete("/users/:id", async (req, res) => {
+  const requestedId = req.params.id;
+  await User.destroy({ where: { id: requestedId } });
+  res.send("Removed");
+});
+
 app.listen(3000, () => {
   console.log("App is running on port 3000");
 });
